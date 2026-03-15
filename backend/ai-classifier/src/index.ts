5dd35bd52b9bd3445e1540b7cfb4d0b2c3144f55
Inesh-Reddy <ineshreddy007@gmail.com>
2026-03-16 05:05:46 +0530

    fixed internal package issue

--- backend/ai-classifier/src/index.ts
+++ backend/ai-classifier/src/index.ts
@@ -1,5 +1,17 @@
+import { TransactionEvent } from "@shared/models";
+
 console.log(`ai classifier`);
 
-function classifyTransactionEvent(event: ) {
+function classifyTransactionEvent(event: TransactionEvent) {
   console.log("Classifying transaction: ", event.signature);
 }
+
+classifyTransactionEvent({
+  event_type: "TRANSACTION_DETECTED",
+  wallet: "574htiugo;Ggbao'sg",
+  signature: "afsgdhf",
+  timestamp: 12345,
+  slot: 123,
+  program: "progmra",
+  raw_transaction: 112,
+});
